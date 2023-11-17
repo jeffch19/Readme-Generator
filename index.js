e9771b7ebb163405f242889bff6e17eb47f054e0
jeffch19 <17jcummings@gmail.com>
2023-11-17 18:08:58 -0500

    initializing starting files

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+//ask the questions here
+let responses = {
+  title: "Turtle"
+}
+  generateMarkdown(responses)
+}
+
+// Function call to initialize app
+init();
